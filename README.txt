fcf119cc5f273b8dba57f1db72868ba360fcc0e5
Nathan Clark <nathanmclark23@gmail.com>
2021-06-13 02:01:31 -0700

    commit from ther computer

---
+++ README.txt
@@ -0,0 +1,59 @@
+pip install -r requirements.txt
+python create_users.py
+python browser_setup.py
+python mine.py
+
+
+pip install pyautogui
+pip install pyclick
+pip install pywin32
+
+
+ git add .
+ git commit -m  ""
+ git push -u origin main   
+
+
+
+
+
+
+
+#  (Shift, Alt, Ctrl)
+
+# YouTube Video: https://www.youtube.com/watch?v=dUeCoUw9OoQ
+# https://datatofish.com/add-python-to-windows-path/
+
+# slmgr.vbs -rearm
+# slmgr -rearm
+
+
+
+
+
+TODO 
+ - use rewards isn't alwats clicked
+ - check that you are vefified as human
+ - claim rewards
+
+
+
+
+
+
+
+
+
+
+ 
+Other Ideas
+  # clear RAM
+  # change IP
+  # hyper-v
+  # random click locations
+  # optimize Time inbetween actions
+  # prepare to run for everyone
+  # don't share data with apps that i have dowbloaded
+  # def delete_user():
+
+
---
+++ create_users.py
@@ -0,0 +1,43 @@
+import win32com.shell.shell as shell
+
+def create_range(start, stop):
+    conglomerate_of_cmd = ''
+    cmd_part_1 = 'C:\WINDOWS\system32\\net.exe user user' 
+    cmd_part_2 = ' 0223 /add '
+    cmd_part_3 = '&& '
+
+    for i in range(start, stop + 1):
+        print(i)
+        conglomerate_of_cmd += cmd_part_1
+        conglomerate_of_cmd += str(i)
+        conglomerate_of_cmd += cmd_part_2
+
+        if i != stop - 1:
+            conglomerate_of_cmd += cmd_part_3
+
+    # print(conglomerate_of_cmd)
+
+    # shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+conglomerate_of_cmd)
+
+
+def run_in_segments(start, stop):
+    start_ = 0
+    stop_ = 0
+    for i in range(start, stop):
+        if (i % 100 == 0 and i != 0) or i == stop - 1:
+            start_ = stop_ + 1
+            stop_ = i
+            # print(start_, " ", stop_)
+            create_range(start_, stop_)
+
+def create_one(user_number):
+    cmd = 'C:\WINDOWS\system32\\net.exe user user' + str(user_number) + ' 0223 /add '
+    shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+cmd)
+
+
+
+start = 1
+stop = 501
+run_in_segments(start, stop)
+
+# create_one(500)
